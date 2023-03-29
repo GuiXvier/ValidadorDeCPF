a29d23774dedacfb93d927d47c5c505f412376a2
GuiXvier <guilherme.contatogg@gmail.com>
2023-03-29 11:49:26 -0300

    Projeto pronto

--- script.js
+++ script.js
@@ -9,12 +9,14 @@ function ValidaCPF(cpfEnviado){
 ValidaCPF.prototype.valida = function(){
     if(typeof this.cpfLimpo === 'undefined') return false;
     if(this.cpfLimpo.length !== 11) return false;
+    if(this.isSequencia()) return false;
 
     const cpfParcial = this.cpfLimpo.slice(0, -2);
     const digito1 = this.criaDigito(cpfParcial);
     const digito2 = this.criaDigito(cpfParcial + digito1);
-    
-    return true
+
+    const novoCpf = cpfParcial + digito1 + digito2;
+    return novoCpf === this.cpfLimpo;
 }
 
 ValidaCPF.prototype.criaDigito = function(cpfParcial) {
@@ -27,11 +29,21 @@ ValidaCPF.prototype.criaDigito = function(cpfParcial) {
     }, 0)
 
     const digito = 11 - (total % 11);
-    return digito > 9 ? 0 : digito;
+    return digito > 9 ?'0' : String(digito);
+}
+
+ValidaCPF.prototype.isSequencia = function() {
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+    return sequencia === this.cpfLimpo;
 }
 
 const cpf = new ValidaCPF('705.484.450-52');
-console.log(cpf.valida())
+
+if (cpf.valida()) {
+    console.log('Cpf válido')
+} else {
+    console.log('Cpf invalido')
+}
 
 
 
